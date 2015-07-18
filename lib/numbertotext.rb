15bdd9daa791218f8ce7061272fbebd3cf15ae11
Edem Kumodzi <ekumodzi@LAekumodzi.local>
2015-07-18 17:10:09 -0400

    Convert 0 to 20, refactored test

--- lib/numbertotext.rb
+++ lib/numbertotext.rb
@@ -4,7 +4,8 @@ module NumberToText
   
   private
   def self.load_dictionary
-    dictionary = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    dictionary = ['zero','one','two','three','four','five','six','seven','eight','nine','ten',
+    	'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty']
     dictionary
   end
 
--- spec/lib/numbertotext_spec.rb
+++ spec/lib/numbertotext_spec.rb
@@ -1,42 +1,19 @@
 require 'spec_helper'
 
 describe NumberToText do
-  it 'should convert 0 to zero' do
-    result = NumberToText.convert(0)
-    expect(result).to eq('zero')
+
+  it 'should convert 0 to 20 to equivalent text' do
+  	ntt = NumberToText
+  	actual = [ntt.convert(0),ntt.convert(1),ntt.convert(2),ntt.convert(3),ntt.convert(4),ntt.convert(5),
+  				ntt.convert(6),ntt.convert(7),ntt.convert(8),ntt.convert(9),ntt.convert(10),
+  					ntt.convert(11),ntt.convert(12),ntt.convert(13),ntt.convert(14),ntt.convert(15),
+  						ntt.convert(16),ntt.convert(17),ntt.convert(18),ntt.convert(19),ntt.convert(20)]
+  	
+  	expected = ['zero','one','two','three','four','five',
+  					'six','seven','eight','nine','ten',
+  						'eleven','twelve','thirteen','fourteen','fifteen',
+  							'sixteen','seventeen','eighteen','nineteen','twenty']
+  	expect(actual).to eq(expected)
   end
 
-  it 'should convert 1 to one' do
-    result = NumberToText.convert(1)
-    expect(result).to eq('one')
-  end
-
-  it 'should convert 2 to two' do
-    result = NumberToText.convert(2)
-    expect(result).to eq('two')
-  end
-
-  it 'should convert 0 to 9 to equivalent text' do
-  	result0 = NumberToText.convert(0)
-  	result1 = NumberToText.convert(1)
-  	result2 = NumberToText.convert(2)
-  	result3 = NumberToText.convert(3)
-  	result4 = NumberToText.convert(4)
-  	result5 = NumberToText.convert(5)
-  	result6 = NumberToText.convert(6)
-  	result7 = NumberToText.convert(7)
-  	result8 = NumberToText.convert(8)
-  	result9 = NumberToText.convert(9)
-
-  	expect(result0).to eq('zero')
-  	expect(result1).to eq('one')
-  	expect(result2).to eq('two')
-  	expect(result3).to eq('three')
-  	expect(result4).to eq('four')
-  	expect(result5).to eq('five')
-  	expect(result6).to eq('six')
-  	expect(result7).to eq('seven')
-  	expect(result8).to eq('eight')
-  	expect(result9).to eq('nine')
-  end
 end
