62fd30d739871ea51dea812625691130faab018c
Edem Kumodzi <ekumodzi@LAekumodzi.local>
2015-07-19 17:10:06 -0500

    Convert 20 to 99

--- lib/numbertotext.rb
+++ lib/numbertotext.rb
@@ -3,15 +3,29 @@ require 'numbertotext/version' unless defined?(NumberToText::VERSION)
 module NumberToText
   
   private
-  def self.load_dictionary
+  def self.load_dictionary1
     dictionary = ['zero','one','two','three','four','five','six','seven','eight','nine','ten',
-    	'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty']
+    	'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
     dictionary
   end
 
+  private
+  def self.load_dictionary2
+    dictionary = ['zero','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+  end
+
+
+
   def self.convert(number)
-    dictionary = load_dictionary
-    dictionary[number]
+    dictionary1 = load_dictionary1
+    if number < 20
+      dictionary1[number]
+    elsif number < 100 && number > 19
+      dictionary2 = load_dictionary2
+      tens = number / 10
+      remainder = number - tens * 10
+      return remainder > 0 ? dictionary2[tens] + '-' + dictionary1[remainder] : dictionary2[tens]
+    end
   end
 
 end
--- spec/lib/numbertotext_spec.rb
+++ spec/lib/numbertotext_spec.rb
@@ -7,13 +7,54 @@ describe NumberToText do
   	actual = [ntt.convert(0),ntt.convert(1),ntt.convert(2),ntt.convert(3),ntt.convert(4),ntt.convert(5),
   				ntt.convert(6),ntt.convert(7),ntt.convert(8),ntt.convert(9),ntt.convert(10),
   					ntt.convert(11),ntt.convert(12),ntt.convert(13),ntt.convert(14),ntt.convert(15),
-  						ntt.convert(16),ntt.convert(17),ntt.convert(18),ntt.convert(19),ntt.convert(20)]
+  						ntt.convert(16),ntt.convert(17),ntt.convert(18),ntt.convert(19)]
   	
   	expected = ['zero','one','two','three','four','five',
   					'six','seven','eight','nine','ten',
   						'eleven','twelve','thirteen','fourteen','fifteen',
-  							'sixteen','seventeen','eighteen','nineteen','twenty']
+  							'sixteen','seventeen','eighteen','nineteen']
   	expect(actual).to eq(expected)
   end
 
+  it 'should convert 20 to twenty' do
+  	actual = NumberToText.convert(20)
+  	expected = 'twenty'
+  	expect(actual).to eq(expected)
+  end
+
+  it 'should convert 21 to twenty-one' do
+    actual = NumberToText.convert(21)
+    expected = 'twenty-one'
+    expect(actual).to eq(expected)
+  end
+
+  it 'should convert 30 to thirty' do
+    actual = NumberToText.convert(30)
+    expected = 'thirty'
+    expect(actual).to eq(expected)
+  end
+
+  it 'should convert 37 to thirty-seven' do
+    actual = NumberToText.convert(37)
+    expected = 'thirty-seven'
+    expect(actual).to eq(expected)
+  end
+
+  it 'should convert 40 to fourty' do
+    actual = NumberToText.convert(40)
+    expected = 'fourty'
+    expect(actual).to eq(expected)
+  end
+
+  it 'should convert 49 to fourty-nine' do
+    actual = NumberToText.convert(49)
+    expected = 'fourty-nine'
+    expect(actual).to eq(expected)
+  end
+
+  it 'should convert 100 to one hundred' do
+    actual = NumberToText.convert(100)
+    expected = 'one hundred'
+    expect(actual).to eq(expected)
+  end
 end
